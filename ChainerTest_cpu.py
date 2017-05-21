fae3903d0b9396cdbc92e51959d05535f5d08e82
TadaoYamaoka <tadaoyamaoka@gmail.com>
2017-05-21 21:08:26 +0900

    ChainerTest_GradWithWeight

---
+++ ChainerTest_cpu.py
@@ -0,0 +1,74 @@
+import numpy as np
+from chainer import Chain, Function, Variable
+from chainer import optimizers
+import chainer.functions as F
+import chainer.links as L
+
+W1 = [[ 1.21082544, -0.42751756],
+      [ 1.35623264, -0.1971387 ],
+      [-0.77883673,  0.28367677]]
+W2 = [[ 0.08621028, -0.19540818,  0.78203094],
+      [ 0.30133799,  1.3698988 , -0.01031571]]
+
+class MyChain(Chain):
+    def __init__(self):
+        super(MyChain, self).__init__(
+            l1=L.Linear(2, 3, initialW=np.array(W1)),
+            l2=L.Linear(3, 2, initialW=np.array(W2)),
+        )
+
+    def __call__(self, x):
+        h = self.l1(x)
+        return self.l2(h)
+
+model = MyChain()
+
+optimizer = optimizers.SGD()
+optimizer.setup(model)
+
+# print param data
+for path, param in model.namedparams():
+    print(path)
+    print(param.data)
+print()
+
+x_data = [[1, 2],
+          [3, 4],
+          [5, 6]]
+t_data = [0, 1, 0]
+z_data = [1.0, 0.5, 0.5]
+
+# init grads
+dic_grads = {}
+for path, param in model.namedparams():
+    dic_grads[path] = []
+
+for x_elem, t_elem in zip(x_data, t_data):
+    x = Variable(np.array([x_elem], dtype=np.float32))
+    t = Variable(np.array([t_elem], dtype=np.int32))
+
+    y = model(x)
+
+    model.cleargrads()
+    loss = F.softmax_cross_entropy(y, t)
+    loss.backward()
+
+    # save grad
+    for path, param in model.namedparams():
+        dic_grads[path].append(param.grad)
+
+# manipulate grad
+z = np.array(z_data, dtype=np.float32)
+for path, param in model.namedparams():
+    grads = np.array(dic_grads[path], dtype=np.float32)
+    grad = np.tensordot(z, grads, axes=1)
+    grad /= len(z_data)
+    param.grad = grad
+
+optimizer.update()
+
+# print param data and grad
+for path, param in model.namedparams():
+    print(path)
+    print(param.data)
+    print(param.grad)
---
+++ ChainerTest_weight.py
@@ -0,0 +1,58 @@
+import numpy as np
+from chainer import Chain, Function, Variable
+from chainer import optimizers
+import chainer.functions as F
+import chainer.links as L
+from softmax_cross_entropy_with_weight import *
+
+W1 = [[ 1.21082544, -0.42751756],
+      [ 1.35623264, -0.1971387 ],
+      [-0.77883673,  0.28367677]]
+W2 = [[ 0.08621028, -0.19540818,  0.78203094],
+      [ 0.30133799,  1.3698988 , -0.01031571]]
+
+class MyChain(Chain):
+    def __init__(self):
+        super(MyChain, self).__init__(
+            l1=L.Linear(2, 3, initialW=np.array(W1)),
+            l2=L.Linear(3, 2, initialW=np.array(W2)),
+        )
+
+    def __call__(self, x):
+        h = self.l1(x)
+        return self.l2(h)
+
+model = MyChain()
+
+optimizer = optimizers.SGD()
+optimizer.setup(model)
+
+# print param data
+for path, param in model.namedparams():
+    print(path)
+    print(param.data)
+print()
+
+x_data = [[1, 2],
+          [3, 4],
+          [5, 6]]
+t_data = [0, 1, 0]
+z_data = [1.0, 0.5, 0.5]
+
+x = Variable(np.array(x_data, dtype=np.float32))
+t = Variable(np.array(t_data, dtype=np.int32))
+z = np.array(z_data, dtype=np.float32)
+
+y = model(x)
+
+model.cleargrads()
+loss = softmax_cross_entropy_with_weight(y, t, z)
+loss.backward()
+
+optimizer.update()
+
+# print param data and grad
+for path, param in model.namedparams():
+    print(path)
+    print(param.data)
+    print(param.grad)
